6b07b0f78ddb6df93ccb2d1578913348d1276461
Jonathan <sifreed77@gmail.com>
2017-10-27 23:37:00 -0400

    Added bubble sort implementation

---
+++ algorithms/bubbleSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// dotnet new console -o bubbleSort
+
+// cd bubbleSort
+
+// dotnet run
+
+namespace bubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bubble Sort Implementation.\n");
+            int[] data = {1,2,4,5,6,7};
+            intArrayBubbleSort(data);
+        }
+
+        static void intArrayBubbleSort(int[] data){
+            int i, j;
+            int N = data.Length;
+
+            for(j=N-1; j>0; j--){
+                for(i=0; i<j; i++){
+                    if(data[i] > data[i + 1]){
+                        exchange(data, i, i+1);
+                    }
+                }
+            }
+        }
+
+        static void exchange (int[] data, int m, int n)
+        {
+            int temporary;
+
+            temporary = data [m];
+            data [m] = data [n];
+            data [n] = temporary;
+            
+        }
+    }
+}
