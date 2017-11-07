0caa75568fb66fa648d61173e20b519d2f32d17b
Jonathan <sifreed77@gmail.com>
2017-11-06 22:01:41 -0500

    Added c# selection sort implementation

---
+++ algorithms/selectionSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace selectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write an example here by calling implementation functions
+        }
+		
+		public static int IntArrayMin(int[] data, int start){
+			int minPos = start;
+			for(int post=start+1; pos < data.Length; pos++){
+				if(data [pos] < data[minPos]){
+					minPos = pos;
+				}
+			}
+			return minPos;
+		}
+		
+		public static void IntArraySelectionSortImpl(int [] data){
+			
+			int i;
+			int N = data.Length;
+			
+			for(i = 0; i < N-1; i++){
+				int k = IntArrayMin(data, i);
+				if(i != k){
+					exchange(data, i, k);
+				}
+			}
+		}
+		
+		public static void exchange (int[] data, int m, int n){
+         
+			int temporary;
+
+			temporary = data [m];
+			data [m] = data [n];
+			data [n] = temporary;
+		}
+    }
+}
