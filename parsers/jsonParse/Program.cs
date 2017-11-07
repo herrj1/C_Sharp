d1efc8d7faca664d17f5558b5812e7a8bc0aeae2
Jonathan <sifreed77@gmail.com>
2017-11-06 22:17:40 -0500

    Added json parsing implementation

---
+++ parsers/jsonParse/Program.cs
@@ -0,0 +1,17 @@
+﻿//Imports from web framework goes here
+using System;
+
+namespace jsonParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exampel1 = @"{""name"": ""John Doe"",""age"":20}";
+			var JSONObj = new JavaScriptSerializer().Deserialize<Dictionary<string, string>> (example1);
+			
+			console.WriteLine("Name: "+ JSONObj["name"]);
+			console.WriteLine("Age: "+ JSONObj["age"]);
+        }
+    }
+}
