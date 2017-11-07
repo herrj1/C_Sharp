9d36df8969bf51e0cc9e2261a4c9b98130911b13
Jonathan <sifreed77@gmail.com>
2017-11-06 22:06:20 -0500

    Added c# insertion sort implementation

---
+++ algorithms/insertionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace insertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write example run usin the functions below.
+        }
+		
+		static void IntArrayInsertionSortImpl(int[] data)
+		{
+			int i, j;
+			int N = data.Length;
+			
+			for(j = 1; j < N; j++)
+			{
+				for(i = j; i > 0 && data[i] < data[i - 1]; i--)
+				{
+					exchange(data, i, i - 1);
+				}
+			}
+		}
+		
+		static void exchange (int[] data, int m, int n){
+         
+			int temporary;
+
+			temporary = data [m];
+			data [m] = data [n];
+			data [n] = temporary;
+		}
+    }
+}
