270d6bd94eb474315e92e52de73429dfd417f157
Jonathan <sifreed77@gmail.com>
2018-01-01 22:38:31 -0500

    Added find max implementation

---
+++ algorithms/singleFileAlgorithms/findMaximum.cs
@@ -0,0 +1,10 @@
+//find maximum. Function only. No driver implemented here.
+static int findMaximum(int[] arr){
+	int max = arr[0];
+	for(int i = 0; i < arr.Length-1; i++){
+		if(arr[index] > max){
+			max = arr[index];
+		}
+		return max;
+	}
+}
