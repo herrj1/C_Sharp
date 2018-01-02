c494500a8826162a1f4aa00531952cb4fca540f7
Jonathan <sifreed77@gmail.com>
2018-01-01 22:37:54 -0500

    Added find minimum implementation

---
+++ algorithms/singleFileAlgorithms/findMinimum.cs
@@ -0,0 +1,10 @@
+//Find mininum. Function only. No driver is implemented.
+static int findMinimum(int[] arr){
+	int min = arr[0];
+	for(int i = 0; i<arr.Length-1; i++){
+		if(arr[index] < min){
+			min = arr[index];
+		}
+	}
+	return min;
+}
