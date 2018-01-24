e5d17c2f00cb59a49fe0d376480168701cf4af5c
Jonathan <sifreed77@gmail.com>
2018-01-24 00:56:54 -0500

    Added arraylist implementation

---
+++ dataStructures/app1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace app1
+{
+    class Program
+    {
+		class Empty{}
+		
+        static void Main(string[] args)
+        {
+            ArrayList numbers = new ArrayList();
+			numbers.Add(33);
+			numbers.Add(56);
+			numbers.Add(92);
+			numbers.Add(45);
+			numbers.Add(10);
+			numbers.Add(new Empty{});
+			
+			foreach(object number in numbers){
+				Console.WriteLine(number);
+			}
+        }
+    }
+}
