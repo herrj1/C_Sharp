358a6f19a524f4c0c4abed3ea3ee411648b9b773
Jonathan <sifreed77@gmail.com>
2018-01-25 23:14:01 -0500

    Added LinkedList implementation

---
+++ dataStructures/app3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace app3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> numbers = new LinkedList<int>();
+			
+			numbers.AddLast(456);
+			numbers.AddLast(146);
+			numbers.AddLast(41);
+			numbers.AddLast(6);
+			numbers.AddLast(1564);
+			
+			LinkedListNode<int> node = numbers.Find(41);
+			numbers.AddBefore(node, 19);
+			
+			foreach(int num in numbers){
+				Console.WriteLine(num);
+			}
+        }
+    }
+}
