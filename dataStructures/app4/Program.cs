8532ebbc16b565b772c45444c7c23d2bd9edc5c7
Jonathan <sifreed77@gmail.com>
2018-01-26 23:50:18 -0500

    Added reversed ArrayList implementation

---
+++ dataStructures/app4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace app4
+{
+    class Program
+    {
+		//This method simply prints all values stored in list.
+		//This method is outside main to avoid clutter. 
+		public static void printValues(IEnumerable langs){
+			int i = 1;
+			foreach(Object obj in langs){
+				Console.WriteLine("{1} {0}", obj, i++);
+			}
+			Console.WriteLine();
+		}
+		
+		
+        static void Main(string[] args)
+        {
+			ArrayList programmingLang = new ArrayList();
+			programmingLang.Add("VB");
+			programmingLang.Add("C++");
+			programmingLang.Add("C#");
+			programmingLang.Add("PHP");
+			programmingLang.Add("J2EE");
+			
+            Console.WriteLine("Starting values of ArrayList: ");
+			printValues(programmingLang);
+			
+			programmingLang.Reverse();
+			
+			Console.WriteLine("ArrayList reversed: ");
+			printValues(programmingLang);
+        }
+    }
+}
