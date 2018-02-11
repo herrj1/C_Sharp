0be960acc4a4c0d84f7d6abe01c0a48e849a9962
Jonathan <sifreed77@gmail.com>
2018-02-11 12:49:57 -0500

    Added pizza decorator pattern implementation

---
+++ handCode/PizzaDecoratorPattern.cs
@@ -0,0 +1,52 @@
+public abstract class Pizza{
+	public abstract double getPrice();
+}
+
+class ThickCrust : Pizza{
+	private double p_Price = 250.0;
+	
+	public override double getPrice(){
+		return p_Price;
+	}
+}
+
+class ThinCrust : Pizza{
+	private double p_Price = 200.0;
+	
+	public override double getPrice(){
+		return p_Price;
+	}
+}
+
+public class Decorator : Pizza{
+	Pizza basePizza = null;
+	
+	protected double p_Price = 0.0;
+	
+	protected Decorator(Pizza obj){
+		basePizza = obj;
+	}
+	public override double getPrice(){
+		return p_Price + basePizza.getPrice();
+	}
+}
+
+public class OnionPizzaDecorator{}
+
+public class CheesePizzaDecorator{}
+
+//Driver
+class PizzaDecoratorPattern{
+	static void Main(string[] args){
+		ThickCrust thickCrust = new ThickCrust();
+		PizzaPriceInfo(thickCrust);
+		
+		OnionPizzaDecorator onionPizza = new OnionPizzaDecorator(thickCrust);
+		
+		CheesePizzaDecorator cheesePizza = new CheesePizzaDecorator(onionPizza);
+		PizzaPriceInfo(cheesePizza);
+	}
+	private static void pizzaPriceInfo(Pizza obj){
+		Console.WriteLine();
+	}
+}
