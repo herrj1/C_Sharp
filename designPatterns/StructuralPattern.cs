54adc8c0eb386f81d1ade0ba9ea1883d7c1785b4
Jonathan <sifreed77@gmail.com>
2018-02-11 12:21:56 -0500

    Added structural pattern implementation

---
+++ designPatterns/StructuralPattern.cs
@@ -0,0 +1,17 @@
+public interface IAdapter{
+	void Add();
+}
+
+public class ClassOne : IAdapter{
+	public void Add(){}
+}
+
+public class ClassTwo{
+	public void Push(){}
+}
+
+public class Adapter : IAdapter{
+	private ClassTwo _class2 = new ClassTwo();
+	
+	public void Add(){ this._class2.Push();}
+}
