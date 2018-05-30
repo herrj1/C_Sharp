1037ef831d55b090ff5569cea83586d22080d99d
Jonathan <sifreed77@gmail.com>
2018-05-29 23:33:34 -0400

    Added studentJsonParse implementation

---
+++ parsers/studentJsonParse/Program.cs
@@ -0,0 +1,37 @@
+﻿//Imports from web framework goes here
+using System;
+
+namespace studentJsonParse{
+	class Program{
+		static void main(string[] args){
+			var student1 = @"{""id"": 1, 
+			                  ""firstname"": ""John"",
+							  ""lastname"": ""Doe"", 
+							  ""college"": ""Acme College"",
+							  ""studentid"": ""vb3234455"" }";
+			var student2 = @"{""id"": 2, 
+			                  ""firstname"": ""Maria"",
+							  ""lastname"": ""Morales"",
+							  ""college"": ""Acme College"",
+							  ""studentid"": ""vb000455"" }";
+			var student3 = @"{""id"": 3, 
+			                  ""firstname"": ""Victor"",
+							  ""lastname"": ""Olives"",
+							  ""college"": ""Acme College"",
+							  ""studentid"": ""vb004355"" }";
+			
+			var JSONObj = new JavaScriptSerializer().Deserialize<Dictionary<string, string>> (student1);
+			console.WriteLine("Name: "+ JSONObj["firstname"] +" "+ JSONObj["lastname"] );
+			console.WriteLine("College: "+ JSONObj["college"] +" ID: "+ JSONObj["studentid"]);
+
+			var JSONObj1 = new JavaScriptSerializer().Deserialize<Dictionary<string, string>> (student2);
+			console.WriteLine("Name: "+ JSONObj1["firstname"] +" "+ JSONObj1["lastname"] );
+			console.WriteLine("College: "+ JSONObj1["college"] +" ID: "+ JSONObj1["studentid"] );
+
+			var JSONObj2 = new JavaScriptSerializer().Deserialize<Dictionary<string, string>> (student3);
+			console.WriteLine("Name: "+ JSONObj2["firstname"] +" "+ JSONObj2["lastname"] });
+			console.WriteLine("College: "+ JSONObj2["college"] +" ID: "+ JSONObj2["studentid"] });			
+			
+		}
+	}
+}
