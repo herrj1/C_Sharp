47d2512e7146fac85aa5d6cdd991b323fd3d6612
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-08-10 23:05:34 -0400

    Update Program.cs

--- databases/sqlClient/Program.cs
+++ databases/sqlClient/Program.cs
@@ -10,7 +10,6 @@ namespace sqlClient
         {
 			string connectionString = "Data Source=(local); Initial Catalog=Northwind; Integrated Security=true";
 			string queryString = "SELECT ProductID, UnitPrice, ProductName from dbo.products WHERE UnitPrice > @pricePoint ORDER by UnitPrice DESC;";
-			
 			int paramValue = 5;
 			
 			using(SqlConnection connection = new SqlConnection(ConnectionString))
